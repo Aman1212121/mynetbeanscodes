b52f894fe799f755def5b1b479adfa2643bcc080
Aman1212121 <bahia7813@gmail.com>
2023-09-16 13:25:34 +0530

    My files to be push

---
+++ file1.java
@@ -0,0 +1,10 @@
+import java.io.File;
+class Test
+{
+    public static void main(String args[])
+    {
+        File ob = new File("myfile.txt");
+        System.out.println("File Created!");
+
+    }
+}
